0de1572bde9775718a560d490f5abfec05d6d0e6
vig98 <vig98@users.noreply.github.com>
2019-08-02 16:09:01 +0530

    TDD unit testing

--- play-samples-play-scala-hello-world-tutorial/test/controllers/getControllerSpec.scala
+++ play-samples-play-scala-hello-world-tutorial/test/controllers/getControllerSpec.scala
@@ -1,6 +1,7 @@
 package controllers
 
 import akka.http.scaladsl.model.HttpHeader.ParsingResult.Ok
+import akka.japi.Option
 import org.scalatest.{FlatSpec, Matchers}
 
 import scala.collection.mutable.Stack
@@ -15,15 +16,18 @@ import org.scalatest.mockito.MockitoSugar
 import play.api.inject.Injector
 import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.test.Helpers._
-
+import play.api.libs.json._
 
 class getControllerSpec extends FlatSpec with Matchers with MockitoSugar with ScalaFutures with GuiceOneAppPerSuite{
   private lazy val appBuilder: GuiceApplicationBuilder = new GuiceApplicationBuilder()
   private lazy val injector: Injector = appBuilder.injector()
   val mockDAO = injector.instanceOf[EmployeeDAO]
   val mockCC =stubControllerComponents()
- // val controller = new getController(mockCC, mockDAO)
 
+  val CREATEcontroller= new createController(mockCC,mockDAO)
+  val DELcontroller= new delController(mockCC,mockDAO)
+  val GETcontroller= new getController(mockCC,mockDAO)
+  val UPDATEcontroller = new updateController(mockCC, mockDAO)
   "getController" should  "take ids which are present" in {
     //val fakeRequest = FakeRequest(GET, "/id")
      // .withHeaders(HOST -> "localhost:8080")
@@ -31,6 +35,7 @@ class getControllerSpec extends FlatSpec with Matchers with MockitoSugar with Sc
     val controller= new getController(mockCC,mockDAO)// should be called SomethingController
 
     val res = controller.get(2).apply(FakeRequest(GET,"/:id"))
+
 
     //status(result) should be (OK)
 
@@ -44,7 +49,7 @@ class getControllerSpec extends FlatSpec with Matchers with MockitoSugar with Sc
     // .withHeaders(HOST -> "localhost:8080")
     val controller= new getController(mockCC,mockDAO)// should be called SomethingController
 
-    val res = controller.get(4).apply(FakeRequest(GET,"/:id"))
+    val res = controller.get(465).apply(FakeRequest(GET,"/:id"))
 
     //status(result) should be (OK)
 
@@ -52,5 +57,27 @@ class getControllerSpec extends FlatSpec with Matchers with MockitoSugar with Sc
      j should be ("null")
 
 
+  }
+
+
+//  "delController" should "not delete when the db is empty" in {
+//
+//    val res = DELcontroller.del(1).apply(FakeRequest(DELETE,"/del/:id"))
+//
+//    val ans =contentAsJson(res).toString()
+//    print(ans)
+//    ans should be ("true")
+//  }
+
+  "createController" should  " created the record " in{
+
+    val json="""{ "FirstName":"srinidhi","LastName":"illa","PhoneNumber":"459"}"""
+
+    val res = FakeRequest(POST, "/post").withJsonBody(Json.parse(json))
+    val resp = route(app,res).get
+
+    val js = contentAsJson(resp)
+    val FirstName = (js \ "FirstName").as[String]
+    FirstName should be ("srinidhi")
   }
 }
